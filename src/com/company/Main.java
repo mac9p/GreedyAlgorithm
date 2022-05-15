f2c8e83aba44dd028819ccfc5100051ece386d44
Zarkii <mleko493@wp.pl>
2022-05-15 16:38:35 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        double liczba;
+        List<Double> listaSkladnikow = new LinkedList<>();
+        liczba = scanner.nextInt();
+
+        metodazachlanna(liczba,listaSkladnikow);
+        System.out.println(listaSkladnikow);
+
+    }
+
+    private static void metodazachlanna(double liczba,List<Double> lista) {
+        if (Math.sqrt(liczba)%1==0){
+            lista.add(Math.sqrt(liczba));
+            return;
+        }
+        double a = (int)Math.sqrt(liczba);
+        while (liczba > 0) {
+            if (a%1==0){
+                liczba -= a*a;
+                lista.add(a);
+            }else
+                a--;
+            }
+    }
+    }
